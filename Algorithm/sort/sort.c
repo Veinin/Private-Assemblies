b190967c733fb8c71e7b55252f364ce1258aa655
Veinin <veininguo@gmail.com>
2015-04-02 17:50:33 +0800

    modify main file

---
+++ Algorithm/sort/sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "sort.h"
+
+int
+main(int argc, char *argv[]) {
+	if(argc !=3) {
+		fprintf(stderr, "usage : ./main [sorttype] [quantity]\n");
+		printf("sort type : \n1.selection sort \n2.insertion sort\n");
+		exit(0);
+	}
+
+	int i, type = atoi(argv[1]), N = atoi(argv[2]);
+	
+	int *array = malloc(sizeof(int) * N);
+	
+	printf("array : \n");
+	srand((unsigned)time(NULL));
+	for(i = 0; i < N; i++) {
+		int number = rand() % 1000;
+		*(array + i) = number;
+		printf("%d ", number);
+	}
+
+	switch(type) {
+	case SELECTION_SORT:
+		selection_sort(array, N);
+		break;
+
+	case INSERTION_SORT:
+		insertion_sort(array, 0, N);
+		break;
+	
+	default:
+		fprintf(stderr, "sort type not find.\n");
+		exit(0);
+	}
+
+	printf("\nafter the sorting of the array :\n");
+	for(i = 0; i < N; i++) {
+		printf("%d ", *(array + i));
+	}
+	printf("\n");
+
+	return 0;
+}
