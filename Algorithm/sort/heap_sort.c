9da743f2eda65eed428a4b9f597f4e57a38b7b44
Veinin <veininguo@gmail.com>
2015-04-15 21:34:42 +0800

    fix heap sort bug

--- Algorithm/sort/heap_sort.c
+++ Algorithm/sort/heap_sort.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
+#include <math.h>
 #include "sort.h"
-
-static void
-printf(int array, int hsz) {
-	int depth = hsz / 2;
-}
 
 void
 max_heapify(int *array, int root, int hsz) {
@@ -12,10 +8,10 @@ max_heapify(int *array, int root, int hsz) {
 	int r = 2 * root + 2;
 	int largest = root;
 
-	if(l < hsz && array[l] > array[root]) 
+	if(l < hsz && array[l] > array[largest]) 
 		largest = l;
 
-	if(r < hsz && array[r] > array[root])
+	if(r < hsz && array[r] > array[largest])
 		largest = r;
 	
 	if(largest != root) {
@@ -27,12 +23,12 @@ max_heapify(int *array, int root, int hsz) {
 void
 heap_sort(int *array, int hsz) {
 	int i;
-	for(i = (hsz - 1) / 2; i >= 0; i--) {
+	for(i = (hsz - 2) / 2; i >= 0; i--) {
 		max_heapify(array, i, hsz);	
 	}
 
 	for(i = hsz - 1; i >= 0; i--) {
 		SWAP(array[0], array[i]);
-		max_heapify(array, 0, i);
+		max_heapify(array, 0, i); 
 	}
 }
