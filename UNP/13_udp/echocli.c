214c2e25a235b9b16bcae1d14c9ea77a7bd58dd5
veinin <veininguo@gmail.com>
2015-09-09 22:47:06 +0800

    hello world

--- UNP/13_udp/echocli.c
+++ UNP/13_udp/echocli.c
@@ -3,14 +3,21 @@
 void
 echo_cli(int sockfd, const struct sockaddr *servaddr, socklen_t servaddrlen)
 {
+	int ret;
 	char sendbuf[1024] = {0};
 	char recvbuf[1024] = {0};
 
 	while (fgets(sendbuf, sizeof(sendbuf), stdin) != NULL)
 	{
-		sendto(sockfd, sendbuf, sizeof(sendbuf), 0, servaddr, servaddrlen);
-		
-		recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, NULL, NULL);
+		sendto(sockfd, sendbuf, strlen(sendbuf), 0, servaddr, servaddrlen);
+
+		ret = recvfrom(sockfd, recvbuf, sizeof(recvbuf), 0, NULL, NULL);
+		if (ret < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			ERR_EXIT("recvfrom");
+		}
 		printf("echo msg : %s", recvbuf);
 
 		memset(&sendbuf, 0, sizeof(sendbuf));
@@ -32,6 +39,8 @@ main(void)
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));	//调用connect，维护本地连接状态
 
 	echo_cli(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr));
 
