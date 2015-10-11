665bdcf25110f1630e2cc1ad7bd2a46e5006f714
Veinin <veininguo@gmail.com>
2015-10-11 19:36:26 +0800

    hello world

--- Algorithm/sort/selection_sort.c
+++ Algorithm/sort/selection_sort.c
@@ -1,3 +1,17 @@
+/**
+ * 选择排序：
+ * 将资料分成已排序、未排序两部分
+ * 依序由未排序中找最小值(or 最大值)，加入到已排序部份的末端
+ * 
+ * 时间复杂度(Time Complexity)，无论要排序数順序如何，都会执行两个回合：
+ * Best Case：Ο(n2)
+ * Worst Case：Ο(n2)
+ * Average Case：Ο(n2)
+ * 
+ * 空间复杂度(Space Complexity)：O(1)
+ *
+ * 稳定性(Stable/Unstable)：不稳定(Unstable)
+ **/
 #include "sort.h"
 
 void
--- Algorithm/sort/shell_sort.c
+++ Algorithm/sort/shell_sort.c
@@ -1,3 +1,26 @@
+/**
+ * 希尔排序
+ * 希尔排序法为插入排序法的改良
+ *
+ * 希尔排序法的概念：
+ * 将资料排列成二维阵列
+ * 对二维阵列的每一行做排序
+ * 利用前一次排序的结果来加快排序
+ *
+ * 希尔排序作法：
+ * 由大到小选定数个间距(Gap)，最后一个Gap一定要是1
+ * 将资料依指定的间距(Gap)分组，进行插入排序
+ * Gap的选择对执行效率有很大的影响
+ *
+ * 时间复杂度(Time Complexity)
+ * 	Best Case：Ο(n)
+ * 	Worst Case：依选用的GAP而定，Ο(n2) ~ Ο(n1.5)
+ * 	Average Case：Ο(n5/4)
+ *
+ * 空间复杂度(Space Complexity)：θ(1)
+ *
+ * 稳定性(Stable/Unstable)：不稳定(Unstable)
+ **/
 #include "sort.h"
 
 void
